4fb409e100c500acbb5d11b0f6a2a60ea4784362
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-02 17:16:43 -0300

    feat: add module jogadores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [],
+  imports: [JogadoresModule],
   controllers: [],
   providers: [],
 })
