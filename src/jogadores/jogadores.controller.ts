6142c36d0b85cab7b22237fb3140deb310cd063b
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-02 18:37:42 -0300

    feat: alter controller

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 @Controller('api/v1/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
-    return JSON.stringify({ nome: 'Henrique' });
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { email } = criarJogadorDto;
+    return JSON.stringify(`{ nome: ${email} }`);
   }
 }
