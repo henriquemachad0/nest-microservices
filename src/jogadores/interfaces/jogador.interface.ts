234096d5da4fec4dca15f7ead5886b8c69942fef
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-04 17:50:28 -0300

    fix: remove _id to interface jogador

--- src/jogadores/interfaces/jogador.interface.ts
+++ src/jogadores/interfaces/jogador.interface.ts
@@ -1,7 +1,6 @@
 import { Document } from 'mongoose';
 
 export interface Jogador extends Document {
-  readonly _id: string;
   readonly telefoneCelular: string;
   readonly email: string;
   nome: string;
