057c1007444378ab5f72bae9b485a300962521c1
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-04 18:38:28 -0300

    feat: add pipes validators in dto

--- src/jogadores/dtos/criar-jogador.dto.ts
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty } from 'class-validator';
+import { IsEmail } from 'class-validator/types/decorator/decorators';
+
 export class CriarJogadorDto {
+  @IsNotEmpty()
   readonly telefoneCelular: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   nome: string;
 }
