60307932a413f5604115bc15a9b970dc0b7945dd
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-09 14:51:51 -0300

    feat: import desafios module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { CategoriasModule } from './categorias/categorias.module';
     ),
     JogadoresModule,
     CategoriasModule,
+    DesafiosModule,
   ],
   controllers: [],
   providers: [],
