e16176460320c103de970571682d2c8a2a7780e2
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-09 15:32:41 -0300

    feat: add consultarCategoriaDoJogador in categorias service

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -50,6 +50,24 @@ export class CategoriasService {
     return categoriaEncontrada;
   }
 
+  async consultarCategoriaDoJogador(idJogador: any): Promise<Categoria> {
+    const jogadores = await this.jogadoresService.consultarTodosJogadores();
+
+    const jogadorFilter = jogadores.filter(
+      (jogador) => jogador._id == idJogador,
+    );
+
+    if (jogadorFilter.length == 0) {
+      throw new BadRequestException(`O id ${idJogador} não é um jogador!`);
+    }
+
+    return await this.categoriaModel
+      .findOne()
+      .where('jogadores')
+      .in(idJogador)
+      .exec();
+  }
+
   async atualizarCategoria(
     categoria: string,
     atualizarCategoriaDto: AtualizarCategoriaDto,
